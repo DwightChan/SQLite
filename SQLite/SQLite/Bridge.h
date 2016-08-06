f2f551b3928ac2e29a410c5b31e75c7b1c398085
chendehao <958689927@qq.com>
2016-08-06 20:50:12 +0800

    添加桥接文件, 并导入 SQLite3的头文件

---
+++ SQLite/SQLite/Bridge.h
@@ -0,0 +1,9 @@
+//
+//  Bridge.h
+//  SQLite
+//
+//  Created by chendehao on 16/8/6.
+//  Copyright © 2016年 CDHao. All rights reserved.
+//
+
+#import <sqlite3.h>
--- SQLite/SQLite/ViewController.swift
+++ SQLite/SQLite/ViewController.swift
@@ -12,12 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // 1.打开数据库方法
+        // 1> 参数一: 文件路径
+        // 2> 参数二: 数据库对象
+        let filePath = "/Users/chendehao/Desktop/mydb.sqlite"
+        let cFilePath = filePath.cStringUsingEncoding(NSUTF8StringEncoding)
+//        if  {
+//            <#code#>
+//        }
+        
+        
+        
     }
 
 
